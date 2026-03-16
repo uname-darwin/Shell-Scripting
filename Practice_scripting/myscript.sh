bbde48e3d5fc234ede8cd0e224cca41501d33fc1
uname-darwin <uname.darwin0@gmail.com>
2026-03-16 06:44:30 +0530

    adding basics

---
+++ Practice_scripting/myscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello World"
+
+echo "I am currently in:"
+pwd
+
---
+++ Practice_scripting/variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+myname="Anitk"
+myage="21"
+
+echo "Hey, I am $myname"
+echo "I can buy beer bacause now I'am $myage"
+
